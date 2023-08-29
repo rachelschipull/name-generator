68bfc1e6dbbed82243205bbf8a8d5b04dcc77bce
Rachel Schipull <rlschipull@gmail.com>
2023-08-29 14:26:53 -0400

    add header and footer components with draft content

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Honorific from '../Honorific'
 import Suffix from '../Suffix'
 import Selector from '../Selector'
+import Header from '../Header'
+import Footer from '../Footer'
 
 function Form () {
   return (
     <>
+      <Header />
       <Honorific />
       <Selector />
       <Suffix />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <>
+            <h1>Startup Baby Name Generator 👼🏼</h1>
+            <p>Works for pets and plants too!</p>
+        </>
+    )
+}
+
+export default Header
