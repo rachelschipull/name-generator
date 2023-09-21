62a2a1a5b7a23ec6c2bca74894d415b1d90d0449
Rachel Schipull <rlschipull@gmail.com>
2023-09-21 15:08:02 -0400

    incomplete randomizer attempt

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import names from './names.js'
 
-function Selector ({onSelectChange}) {
+function Selector () {
+
+  const [selectedNumber, setSelectedNumber] = React.useState('');
+
 
   return (
     <form>
@@ -11,8 +15,9 @@ function Selector ({onSelectChange}) {
         
         <select
           className='select select-primary w-full max-w-xs'
+          value={selectedNumber}
           onChange={event => {
-            onSelectChange(event.target.value)
+            setSelectedNumber(event.target.value)
           }}
         >
           <option value='1'>
@@ -30,5 +35,25 @@ function Selector ({onSelectChange}) {
     </form>
   )
 }
+
+function shuffle(){
+
+  //names array, shuffle it, then slice [selecteValue-1], join, toString, export to Form
+  
+  //(selectedValue - 1), shuffle names array, take the 
+
+  const fyshuffle = (names) => {
+    let oldElement;
+    for (let i = array.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      oldElement = array[i];
+      array[i] = array[rand];
+      array[rand] = oldElement;
+    }
+    names.slice((selectedNumber - 1))
+  } 
+}
+
+
 
 export default Selector;
