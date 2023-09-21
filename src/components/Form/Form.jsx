da4517c560e96f23e703abcfdf0dbb8f4956c425
Rachel Schipull <rlschipull@gmail.com>
2023-09-21 15:04:27 -0400

    trying to add randomizer logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,17 +15,14 @@ function Form () {
     setSuffData(newSuff)
   }
 
-  const [selectedNumber, setSelectedNumber] = useState('');
-  const handleSelectChange = (newNumber) => {
-    setSelectedNumber(newNumber)
-  }
+
 
   return (
     <div className="w-screen h-screen">
         <Header />
           <div className="flex justify-center justify-between px-40 w-100 m-5">
             <Honorific onDataChange={handleHonorChange}/>
-            <Selector onSelectChange={handleSelectChange}/>
+            {/* <Selector onSelectChange={handleSelectChange}/> */}
             <Suffix onSuffChange={handleSuffChange}/>
           </div>
           <div className="flex justify-center mb-5">
